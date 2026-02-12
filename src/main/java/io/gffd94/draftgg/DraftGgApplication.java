2b9cc0803f42d0d702f0ab18c6f7475f5a06cffb
STGRAM\gffd9 <29088981l@gmail.com>
2026-02-13 00:39:42 +0900

    feat: [Init] 프로젝트 초기 설정 및 의존성 추가

---
+++ src/main/java/io/gffd94/draftgg/DraftGgApplication.java
@@ -0,0 +1,13 @@
+package io.gffd94.draftgg;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DraftGgApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DraftGgApplication.class, args);
+    }
+
+}
---
+++ src/test/java/io/gffd94/draftgg/DraftGgApplicationTests.java
@@ -0,0 +1,13 @@
+package io.gffd94.draftgg;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DraftGgApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
